25977ac73ec03e40ffbf059a448384bb95cf07b1
Luis Furtado <lucho_l.f@hotmail.com>
2023-10-14 10:19:25 -0300

    agregando platillos

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ let cliente = {
     pedido: []
 };
 
+const catergorias =  {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -61,6 +67,116 @@ function obtenerPlatillos() {
 
     fetch(url)
         .then( respuesta => respuesta.json() )
-        .then( resultado => console.log(resultado) )
+        .then( resultado => mostrarPlatillos(resultado) )
         .catch( error => console.log(error))
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const catergoria = document.createElement('DIV');
+        catergoria.classList.add('col-md-3');
+        catergoria.textContent = catergorias[ platillo.categoria ];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+
+        // Funcion que detecta la cantidad y el platillo q se esta agregando
+        inputCantidad.onchange = function() {
+            const cantidad = parseInt(  inputCantidad.value );
+            agregarPlatillo({...platillo, cantidad});
+        };
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(catergoria);
+        row.appendChild(agregar);
+
+        contenido.appendChild(row);
+    });
+}
+
+function agregarPlatillo(producto) {
+    // Extrae el pedido actual
+    let { pedido } = cliente;
+
+    // Revisar que la cantidad se mayor a 0
+    if (producto.cantidad > 0) {
+        
+        // Comprueba si el elemento ya existe en el array
+        if ( pedido.some( articulo => articulo.id === producto.id ) ) {
+            // El artidulo ya existe, Actualizar la cantidad
+            const pedidoActualizado = pedido.map( articulo => {
+                if( articulo.id === producto.id ) {
+                    articulo.cantidad = producto.cantidad;
+                }
+                return articulo;
+            });
+            // Se asigna el nuevo array a cliente.pedido
+            cliente.pedido = [...pedidoActualizado];
+        } else {
+            // El Articulo no existe, lo agregamos al larray del pedido
+            cliente.pedido = [...pedido, producto];
+        }
+    } else{
+        // Eliminar elemento cunado la cantidad es 0
+        const resultado = pedido.filter( articulo => articulo.id !== producto.id );
+        cliente.pedido = [...resultado];
+    }
+
+    // Mostrar el resumen
+    actualizarResumen()
+}
+
+function actualizarResumen() {
+
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const resumen = document.createElement('DIV');
+    resumen.classList.add('col-md-6');
+
+    // Información de la mesa
+    const mesa = document.createElement('P');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold');
+
+    const mesaSpan = document.createElement('SPAN');
+    mesaSpan.textContent = cliente.mesa;
+    mesaSpan.classList.add('fw-normal');
+
+    // Información de la hora
+    const hora = document.createElement('P');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold');
+
+    const horaSpan = document.createElement('SPAN');
+    horaSpan.textContent = cliente.hora;
+    horaSpan.classList.add('fw-normal');
+
+    // Agregar a los elementos padres
+    mesa.appendChild(mesaSpan);
+    hora.appendChild(horaSpan);
+
+    contenido.appendChild(mesa);
+    contenido.appendChild(hora);
 }
